c8aa0ec5f08f1437c032174c403af47a332f08d6
Cini <dominicho002@gmail.com>
2026-06-02 13:02:13 -0400

    Added images to home page Edited Structure of projects page Added structure to about me page

--- project.js
+++ project.js
@@ -1,9 +1,14 @@
-const starButton = document.querySelectorAll("Stars");
-
+const starButton = document.getElementById("Stars");
+const starbuttongPrompt = prompt(starButton);
 starButton.addEventListener("click", SpawnDrawTheNightSky);
-
+const lightSwinger = document.getElementById("light");
+lightSwinger.addEventListener("click", SpawnLightSwinger);
+function SpawnLightSwinger()
+{
+    lightSwinger.style.display ='none';
+}
 function SpawnDrawTheNightSky()
 {
     var element = document.getElementById("Stars")
-    element.style.display ="none";
+    element.style.display ='none';
 }
