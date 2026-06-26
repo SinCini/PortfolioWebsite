166a37985180941d427a39abcfc9744121cf6b25
Cini <dominicho002@gmail.com>
2026-06-25 20:19:00 -0400

    Buttons in portfolio links are fixed General Scheme of the website changed

--- project.js
+++ project.js
@@ -5,9 +5,27 @@ lightSwinger.addEventListener("click", SpawnLightSwinger);
 function SpawnLightSwinger()
 {
     lightSwinger.style.display ='none';
+    
+    const lightGameContainer = document.createElement("div");
+    const lightswingerGame = document.createElement("iframe");
+    lightswingerGame.className = "LightGameCanvas";
+    lightswingerGame.src="./ProjectFiles/Lightswinger/index.html"; 
+    lightswingerGame.width="100%";
+    lightswingerGame.height="1000"; 
+    lightswingerGame.title="Light"
+    const game = document.getElementById("LightGame");
+    game.appendChild(lightswingerGame);
 }
 function SpawnDrawTheNightSky()
 {
-    var element = document.getElementById("Stars")
-    element.style.display ='none';
+    starButton.style.display ='none';
+
+    const lightGameContainer = document.createElement("div");
+    const NightSkyGame = document.createElement("iframe");
+    NightSkyGame.className = "SkyGameCanvas";
+    NightSkyGame.src="./ProjectFiles/DrawTheNightSky/index.html";
+    NightSkyGame.width="100%";
+    NightSkyGame.height="100%"; 
+    NightSkyGame.title="Stars"
+     const game = document.getElementById("StarGame");
 }
